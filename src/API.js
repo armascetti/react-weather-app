13d725a73a9c6ccb10898e8b9e5db8092b063d05
Mandy_Mascetti <armascetti@gmail.com>
2022-08-16 22:47:48 -0400

    initial commit/fetch API data

---
+++ src/API.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '44a5c6dafbmshe3378ee25674913p1691c8jsn320bdb811cd2',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
